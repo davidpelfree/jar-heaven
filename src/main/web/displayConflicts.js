87a6a725a6e34264e159717d8251ee4ea0f35f6e
Ofer K <ofer.k@tikalk.com>
2016-07-19 14:36:58 +0300

    1

--- src/main/web/displayConflicts.js
+++ src/main/web/displayConflicts.js
@@ -1,20 +1,27 @@
+$(document).ready(function() {
+_.each(conflictsJson.conflicts,function(conflictsType,conflictTypeName){
 
-_.each(conflictsJson.conflicts,function(conflicts){
+    _.each(conflictsType,function(conflict){
 
-    _.each(conflicts,function(conflictType, conflictTypeName){
-        
-        _.each(conflictType,function(conflict) {
-            var tRow = '<th scope="row">'+conflictTypeName+'</th>';
-            var name = Object.keys(conflict)[0];
-            tRow += "<td>"+name+"</td>";
-            tRow += "<td>"+name+"</td>";
-            debugger;
+        var conflictName = Object.keys(conflict)[0];
+        var tRow = '<th scope="row">'+conflictTypeName+'</th>';
+        var name = Object.keys(conflict)[0];
+        tRow += "<td>"+name+"</td>";
+        var issues = "";
+        issues += "<div>";
+        _.each(conflict[conflictName],function(detail) {
+            _.each(detail,function(detailValue,detailName) {
+                detailName = detailName.replace("_"," ")
+                issues += "<p><b>"+detailName+":</b> "+detailValue+"</p>";
+            })
         })
-            
-        
-        
+        issues += "</div>";
+        tRow += "<td>"+conflict[conflictName].length+"</td>";
+        tRow += "<td>"+issues+"</td>";
+        $("table tbody").append("<tr>"+tRow+"</tr>");
     })
 })
+});
 
 // <tr>
 // <th scope="row">1</th>
